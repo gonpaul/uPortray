b74761b7e4897d6653730b97a1a6d701a856c0a5
gonpaul <pavelgoncharov56@gmail.com>
2024-02-09 22:03:48 +0300

    migrated ChatItem to a separated component

---
+++ src/components/ChatItem.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
+
+
+const ChatItemList = styled.div`
+  border-radius: 20px;
+  background-color: var(--ash-brown, #59473f);
+  padding: 18px 60px;
+  font-size: 18px;
+  color: var(--light-grey, #ecebe9);
+
+  &:hover {
+    background-color: var(--morris-room-grey, #a59a8c);
+  }
+`;
+
+interface Chat {
+  id: number;
+  name: string;
+  messages: string[];
+}
+
+interface ChatItemProps {
+  chat: Chat;
+  onChatNameChange: (id: number, newName: string) => void;
+}
+
+const ChatItem: React.FC<ChatItemProps> = ({ chat, onChatNameChange }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [chatName, setChatName] = useState(chat.name);
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChatName(event.target.value);
+    onChatNameChange(chat.id, event.target.value);
+  };
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <ChatItemList onDoubleClick={handleDoubleClick}>
+      {isEditing ? (
+        <input
+          type="text"
+          value={chatName}
+          onChange={handleNameChange}
+          onBlur={handleBlur}
+          autoFocus
+        />
+      ) : (
+        <span>{chatName}</span>
+      )}
+    </ChatItemList>
+  );
+}
+
+export default ChatItem;
